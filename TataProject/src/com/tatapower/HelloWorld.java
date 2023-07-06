98acf8d5d67f551386c036e0c383e6c3a135804a
Shubham <shubhamhingadesw@gmail.com>
2023-07-06 08:44:35 +0000

    Second Commit

---
+++ TataProject/src/com/tatapower/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.tatapower;
+
+public class HelloWorld {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Tata Power");
+	}
+}
